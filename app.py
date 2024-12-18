494016b27f28a53d183dad85e673e71c000937d7
gabriel <gabaamazing21@gmail.com>
2024-12-18 07:37:00 +0100

    added database

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#configure sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notifications.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 @app.route('/')
 def home():
     return "welcome to the custom notification services!"
